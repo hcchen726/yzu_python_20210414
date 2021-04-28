8788d7af51224a83d8a35c821e4f3befaab80978
hcc <hcchen726@gmail.com>
2021-04-28 19:26:22 +0800

    python

---
+++ day03/BMI輸入.py
@@ -0,0 +1,11 @@
+# -*- coding:UTF-8 -*-
+h = input('請輸入身高:')
+w = input('請輸入體重:')
+print(h, type(h))
+print(w, type(w))
+h = float(h)  # 將str轉成float
+w = float(w)  # 將str轉成float
+print(h, type(h))
+print(w, type(w))
+bmi = w / ((h/100) ** 2)
+print("%.2f" % bmi)
