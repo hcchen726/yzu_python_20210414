ba260a3527a6fc366d64d4462295756ff0731e7f
hcc <hcchen726@gmail.com>
2021-04-28 19:48:37 +0800

    python

--- day03/BMI輸入2.py
+++ day03/BMI輸入2.py
@@ -5,3 +5,9 @@ h = float(input('請輸入身高:'))
 w = float(input('請輸入體重:'))
 bmi = w / math.pow(h/100, 2)
 print('BMI計算成果: %.2f' % bmi)
+if bmi > 23:
+    print("過重")
+elif bmi <= 18:
+    print("過輕")
+else:
+    print("正常")
---
+++ day03/邏輯測試.py
@@ -0,0 +1,3 @@
+b = True or False and False # and 比 or 先做
+print(b)
+# https://www.mathcs.emory.edu/~valerie/courses/fall10/155/resources/op_precedence.html
